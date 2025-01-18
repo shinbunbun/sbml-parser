90a15061b62c6c83483ae88ed0ccefa3fb3f185e
shinbunbun <34409044+shinbunbun@users.noreply.github.com>
2025-01-18 23:17:12 +0900

    fix

--- src/models/concrete/list_of_event_assignments.rs
+++ src/models/concrete/list_of_event_assignments.rs
@@ -1,10 +1,14 @@
 use serde::{Deserialize, Serialize};
 
+use crate::models::r#abstract::s_base::SBase;
+
+use super::event_assignment::EventAssignment;
+
 // p79/Section4.12
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 pub struct ListOfEventAssignments {
     #[serde(rename = "eventAssignment")]
     pub event_assignment: Vec<EventAssignment>,
     #[serde(flatten)]
-    pub s_base: SBasee,
+    pub s_base: SBase,
 }
