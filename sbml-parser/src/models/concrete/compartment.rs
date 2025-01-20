8ad7b34616b6d024c1791144e0305e2d24d5aa32
Hina524 <132186412+Hina524@users.noreply.github.com>
2025-01-20 23:16:31 +0900

    add@/Compartment

--- sbml-parser/src/models/concrete/compartment.rs
+++ sbml-parser/src/models/concrete/compartment.rs
@@ -6,12 +6,14 @@ use crate::models::{primitive::SId, r#abstract::s_base::SBaseAttributes};
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 pub struct Compartment {
     pub id: SId,
-    #[serde(rename = "spatialDimensions", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@spatialDimensions", skip_serializing_if = "Option::is_none")]
     pub spatial_dimensions: Option<f64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@size", skip_serializing_if = "Option::is_none")]
     pub size: Option<f64>,
+    #[serde(rename = "@units")]
     pub units: String,
     ///  Compartment also has a mandatory boolean attribute called constant that indicates whether the compart-40 ment’s size stays constant or can vary during a simulation. A value of “false” indicates the compartment’ size can be changed by other constructs in SBML. A value of “true” indicates the compartment’s size1 cannot be changed by any construct except InitialAssignment.
+    #[serde(rename = "@constant")]
     pub constant: bool,
 }
 
