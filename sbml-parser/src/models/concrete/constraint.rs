4d353f126f00d074c595a9b15621bcec7443ed08
Hina524 <132186412+Hina524@users.noreply.github.com>
2025-01-20 23:21:09 +0900

    add@/constraint

--- sbml-parser/src/models/concrete/constraint.rs
+++ sbml-parser/src/models/concrete/constraint.rs
@@ -10,13 +10,13 @@ use super::{math::Math, message::Message};
 // Section 4.10
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 pub struct Constraint {
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@id", skip_serializing_if = "Option::is_none")]
     pub id: Option<ID>,
-    #[serde(rename = "sboTerm", skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "@sboTerm", skip_serializing_if = "Option::is_none")]
     pub sbo_term: Option<SBOTerm>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename = "math", skip_serializing_if = "Option::is_none")]
     pub math: Option<Math>,
-    #[serde(skip_serializing_if = "Option::is_none")]
+    #[serde(rename ="message", skip_serializing_if = "Option::is_none")]
     pub message: Option<Message>,
 }
 
